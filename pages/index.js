8f8604bbd6fab11a72e6c45f3c23cdb519656095
Ron Dyar <ron@fromex.net>
2022-11-26 19:45:21 -0800

    edit

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ export default function index() {
     <>
       <h1>NextWindStarter</h1>
       <p>Set password in api/auth [...nextauth]</p>
+      <p>
+        In order for it to work when deployed you need to set a env for
+        NEXT_PUBLIC_SECRET=anyrandomstring.
+      </p>
     </>
   )
 }
